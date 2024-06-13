2208935d13fb7f7e85a827ac779e91323576f78c
gdm-9961 <159160385+gdm-9961@users.noreply.github.com>
2024-06-13 15:03:26 -0400

    Change albums loading
    Wrote javascript to generate covers from tsv

--- sub/albums.js
+++ sub/albums.js
@@ -1,13 +1,33 @@
 window.addEventListener("load",setup);
 
 async function setup(){
-		songpaths = (await rawData("songpaths.txt")).split("\n");
+		let tempdata = (await rawData("songpaths.tsv")).split("\n");
+		songdata = [];
+		for (let l = 0; l < tempdata.length; l++){
+			songdata.push(tempdata[l].split("\t"));
+		}
+		library = document.getElementById("covers");
 		band = document.getElementById("songartist");
 		song = document.getElementById("songtitle");
 		player = document.getElementById("player");
-		console.log(songpaths);
+		library.innerHTML = buildCovers();
+		console.log(songdata);
 		albums = document.getElementsByClassName('cover');
 		console.log(albums);
+}
+
+function buildCovers(){
+	let songs = [];
+	let num = 0;
+	for (album of songdata){
+		songs.push(`<li id="${album[0]}">
+		<img class="cover" src="../media/${album[3]}" alt="${album[0]} - ${album[3]} album cover" onclick="setSong(${num})">
+		<album ${(album.length < 7) ? 'class="normal"' : ('class="long" title="' + album[6] + '"')}>${album[1]}</album><br><artist>${album[0]}</artist><br>
+		[<a href="${album[5]}" target="_blank">link</a>]
+	</li>`);
+	num++;
+	}
+	return songs.join("");
 }
 
 async function rawData(fileIn = ""){
@@ -18,10 +38,8 @@ async function rawData(fileIn = ""){
 }
 
 function setSong(path){
-	console.log(path*2);
-	let idText = songpaths[path*2].split("-");
-	band.textContent = idText[0] + " - ";
-	song.textContent = idText[1];
-	player.src = songpaths[path*2+1];
+	band.textContent = songdata[path][0] + " - ";
+	song.textContent = songdata[path][2];
+	player.src = `../media/${songdata[path][4]}`;
 	if (player.style.display == "none") player.style.display = "inline-block";
 }
