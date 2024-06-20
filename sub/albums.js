04886ba7436c850d393bd9e6d74ff220dbd4d374
gdm-9961 <gdmeldrumnfhs@gmail.com>
2024-06-20 08:24:30 -0400

    Reorganize directory
    Moved img and audio files to respective folders under media

--- sub/albums.js
+++ sub/albums.js
@@ -25,7 +25,7 @@ function buildCovers(){
 	let num = 0;
 	for (album of songdata){
 		songs.push(`<li id="${album[0]}">
-		<img class="cover" src="../media/${album[3]}" alt="${album[0]} - ${album[3]} album cover" onclick="setSong(${num})">
+		<img class="cover" src="../media/img/${album[3]}" alt="${album[0]} - ${album[3]} album cover" onclick="setSong(${num})">
 		<album ${(album.length < 7) ? 'class="normal"' : ('class="long" title="' + album[6] + '"')}>${album[1]}</album><br><artist>${album[0]}</artist><br>
 		[<a href="${album[5]}" target="_blank">link</a>]
 	</li>`);
@@ -44,6 +44,6 @@ async function rawData(fileIn = ""){
 function setSong(path){
 	band.textContent = songdata[path][0] + " - ";
 	song.textContent = songdata[path][2];
-	player.src = `../media/${songdata[path][4]}`;
+	player.src = `../media/audio/${songdata[path][4]}`;
 	if (player.style.display == "none") player.style.display = "inline-block";
 }
